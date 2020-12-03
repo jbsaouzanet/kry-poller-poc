369a531a643d84ad879f5ed575fb9f99be1e8460
JBS <jbsaouzanet@gmail.com>
2020-12-03 20:02:24 +0100

    00004 - Update CSS, HomePage, HardCoded backEnd Object

--- src/main/webapp/component/home/HomePage.js
+++ src/main/webapp/component/home/HomePage.js
@@ -1,7 +1,59 @@
 import React from "react";
 
 const HomePage = () => {
-    return <div></div>;
+    const homeData = [
+        {
+            "name": "Service 1",
+            "status": {
+                "name": "Google",
+                "url": "http://google.Fr",
+                "addTime": {
+                    "epochSecond": 1607021488,
+                    "nano": 4000000
+                },
+                "status": "FAIL"
+            }
+        },
+        {
+            "name": "Service 2",
+            "status": {
+                "name": "test",
+                "url": "http://localhost:3000/#/test",
+                "addTime": {"epochSecond":1607021488,"nano":154000000},
+                "status": "UNKNOWN"
+            }
+        },
+        {
+            "name": "Service 2",
+            "status": {
+                "name": "create",
+                "url": "http://localhost:3000/#/create",
+                "addTime": {"epochSecond":1607021488,"nano":154000000},
+                "status": "OK"
+            }
+        }
+    ]
+    return (
+        <div className="box-content-container">
+            {homeData.map((url, index) => (
+                <div className="row" key={index}>
+                    <div className="col">
+                        <h4>{url.status.status}</h4>
+                    </div>
+                    <div className="col">
+                        <h4>{url.name}</h4>
+                    </div>
+                    <div className="col">
+                        <a href={url.status.url}>TEST</a>
+                    </div>
+                    <div className="col">
+                        <h4>supprimer</h4>
+                    </div>
+                </div>
+            ))
+            }
+        </div>
+    );
 }
 
 export default HomePage;
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router } from "react-router-dom";
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
