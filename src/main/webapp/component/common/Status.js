65547638c0959e19125ea9835b76a53d0028b8de
JBS <jbsaouzanet@gmail.com>
2020-12-03 20:25:49 +0100

    00005 - Create Status Component

---
+++ src/main/webapp/component/common/Status.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Status = ({ status }) => {
+    let colorCode = "red";
+    if(status === "OK") {
+        colorCode = "green";
+    } else if (status === "UNKNOWN") {
+        colorCode = "orange";
+    } else {
+        colorCode = "red";
+    }
+
+   return(
+       <svg height="100" width="100">
+        <circle cx="50" cy="50" r="40" stroke="" stroke-width="3" fill={colorCode} />
+        Sorry, your browser does not support inline SVG.
+       </svg> );
+
+};
+export default Status;
