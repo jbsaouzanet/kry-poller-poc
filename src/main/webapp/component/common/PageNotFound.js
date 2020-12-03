9382f4a9a0505d41853b7312dfed6d126e9beef3
JBS <jbsaouzanet@gmail.com>
2020-12-03 19:34:49 +0100

    00003 - Folder Reorganization, React Router, HomePage creation

---
+++ src/main/webapp/component/common/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <div><h1>Sorry! Page not found.</h1></div>;
+
+export default PageNotFound;
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { HashRouter as Router } from "react-router-dom";
+import App from './component/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <App />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
